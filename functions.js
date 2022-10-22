63c3cfc893dff05d79589135a9fb5213bc92bc3c
Lauren Seward <lauren.unal@gmail.com>
2022-10-21 20:34:42 -0500

    Update responses in functions exercises

--- functions.js
+++ functions.js
@@ -1,11 +1,26 @@
 // 1: Write a function named printGreeting that prints a simple greeting message, the same one, every time it is called. Call this function 3 times.
 
+function printGreeting() {
+    console.log("Welcome! We are happy to meet you!");
+}
+printGreeting();
+printGreeting();
+printGreeting();
+
 
 // 2: Write a function that accepts 1 argument. The function should console.log a sentence that interpolates the data passed in.
 
+function printFarewell(name) {
+    console.log(`Hate to see you go ${name}!`);
+  }
+  printFarewell("James");
 
 // 3: Write a function that has 3 parameters: a string and two numbers. The String will be the name of a company, and the numbers will represent the minimum and maximum of a pay range for a posted job. The function should print out a sentence that includes the name of the company and the range itself (if the numbers passed in are 90000 and 110000, the pay range is 20000).
-
+function printPayRange(company, max, min) {
+  var range = max - min;
+  console.log(`The current pay range at ${company} is ${range}.`);
+}
+printPayRange("Kitty Cat Co", 95, 75);
 
 // 4: Write a function that satifies the following interaction pattern:
 
@@ -20,3 +35,15 @@ checkStock(0, "Cheese");
 
 checkStock(1, "Salsa");
 // => "Salsa - running LOW"
+
+function checkStock(quantity, item) {
+    if (quantity >= 4 && item === "Coffee") {
+      console.log(`${item} is stocked`);
+    } else if (quantity <=3 && item === "Tortillas") {
+      console.log(`${item} - running LOW`);
+    } else if (quantity <= 0 && item === "Cheese") {
+      console.log(`${item} - OUT of stock!`); 
+    } else if (quantity <= 1 && item === "Salsa") {
+      console.log(`${item} - running LOW`);
+    }
+  }
